a6279e6030e902bd4de01c1569e9700109a6827e
Marat <eshmakar@mail.ru>
2021-02-09 22:55:09 +0300

    Initial commit

---
+++ src/test/java/com/example/sheduledtask/SheduledtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sheduledtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheduledtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
